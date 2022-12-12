393bf148e13df9305a56ce8888c071ee9c38401f
Vue.js2 <hiroki.k.0323@docomo.ne.jp>
2022-12-12 15:18:42 +0900

    12/12

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/login">Login</router-link> |
-    <router-link to="/sandbox/313">sandBox</router-link> |
-    <router-link to="/sandbox/さんびゃくじゅうさん">sandBox2</router-link> |
-  </nav>
-  <router-view/>
+  <Header />
 </template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Header
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/Header.vue
@@ -0,0 +1,67 @@
+<template>
+  <header>
+    <div id="header-top">
+    <img alt="Vue logo" src="../assets/キャリーバッグ.jpeg">
+    <h1>お出かけ支援サイト</h1>
+    </div>
+    <nav>
+      <router-link to="/">トップ</router-link> |
+      <router-link to="/about">おでかけ</router-link> |
+      <router-link to="/login">経路検索</router-link> |
+      <router-link to="/sandbox/313">店舗検索</router-link> |
+      <router-link to="/sandbox/さんびゃくじゅうさん">カレンダー</router-link> |
+      <router-link to="/sandbox/さんびゃくじゅうさん"></router-link> 
+    </nav>
+  </header>
+</template>
+
+
+<style scoped>
+header{
+    background: rgb(183, 183, 223);
+    padding:0%;
+    margin: 0%;
+}
+img{
+  height: 20px;
+}
+nav{
+border-bottom: 5px solid #ccc;
+position: relative;
+}
+nav ul{
+position: absolute;
+left:10%;
+display: table;
+margin: 0 auto;
+padding: 0;
+width: 80%;
+text-align: center;
+}
+nav ul li{
+display: table-cell;
+min-width: 50px;
+}
+nav ul li a{
+display: block;
+width: 100%;
+height: 100%;
+padding: 10px 0;
+text-decoration: none;
+color: #aaa;
+}
+nav ul li.current{
+font-weight: bold;
+border-top: 5px solid #00B0F0;
+}
+nav ul li.current a{
+color: #00B0F0;
+}
+nav ul li a:hover{
+color: #0089BB;
+background-color: #FBFBDD;
+}
+nav ul li:hover{
+border-top: 5px solid #F8E750;
+}
+</style>
